29981da221272c9ee4d727d3d92ea69a1e400cce
btiac1129 <btiac1129@gmail.com>
2021-03-01 11:15:29 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// TODO: TodoList 구현
+// FIXME: 
+function TodoList() {
+  const title = '...';
+  const name = '...';
+}
 
 function App() {
   return (
@@ -7,17 +15,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code> src / App.js </code> and save to reload.{' '}
+        </p>{' '}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{' '}
+        </a>{' '}
+      </header>{' '}
     </div>
   );
 }
